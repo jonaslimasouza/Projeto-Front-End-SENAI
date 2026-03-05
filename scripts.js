624b29c412fed9bd51bcaf9c63b7e77c30f4882a
Elvizion <gomesfarias1414@gmail.com>
2026-03-05 19:41:06 -0300

    asa

--- scripts.js
+++ scripts.js
@@ -1,3 +1,32 @@
 function inscrever() {
     alert("Matrícula Efetuada com Sucesso!");
 }
+
+const cepInput = document.getElementById("cep");
+
+// quando o usuário sair do campo CEP
+cepInput.addEventListener("blur", function() {
+
+    let cep = cepInput.value.replace("-", "");
+
+    let url = "https://viacep.com.br/ws/" + cep + "/json/";
+
+    fetch(url)
+    .then(function(resposta){
+        return resposta.json();
+    })
+    .then(function(dados){
+
+        // coloca os dados dentro dos inputs do HTML
+        document.getElementById("rua").value = dados.logradouro;
+        document.getElementById("bairro").value = dados.bairro;
+        document.getElementById("cidade").value = dados.localidade;
+        document.getElementById("estado").value = dados.uf;
+
+    })
+    //Catch evitar que o site quebre caso algo dê errado ao buscar o CEP.
+    .catch(function(){
+        alert("Erro ao buscar CEP");
+    });
+
+});
